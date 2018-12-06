ca419e288b022849a5cd91df92582182a3b68e82
PiPi <1303798456@qq.com>
2018-12-06 13:27:53 +0800

    init

---
+++ db.js
@@ -0,0 +1,35 @@
+﻿// JavaScript source code
+"use strict";
+
+var mysql = require("mysql");
+
+var pool = mysql.createPool({
+    connectionLimit: 5,
+    host: 'localhost',
+    user: 'root',
+    password: 'fy121212', 
+    database: 'app_thy'
+});
+
+var DB = {
+  'start': function(req,res) {
+    pool.getConnection(function(err, con) {
+        if (err) throw err;
+        con.query("SELECT * FROM start", function(err, result) {
+            return res.jsonp(result);
+        });
+        con.release();
+      });
+  },
+  'App': function(req,res,sheet_name) {
+    pool.getConnection(function(err, con) {
+        if (err) throw err;
+        con.query(`SELECT * FROM ${sheet_name}`, function(err, result) {
+            return res.jsonp(result);
+        });
+        con.release();
+      });
+  }
+};
+
+exports = module.exports = DB;
---
+++ router.js
@@ -0,0 +1,15 @@
+﻿const express = require("express");
+const DB = require("./db");
+const router = express.Router();
+
+router.get("/all", (req, res) => {
+  //DB.start(req,res);
+  res.send('测试OK');
+});
+
+router.get("/select", (req, res) => {
+  const sheet_name = req.query.sheet_name;
+  DB.App(req,res,sheet_name);
+});
+
+module.exports = router;
