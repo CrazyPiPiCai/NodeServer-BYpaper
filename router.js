85336e49c630968ad884492b387f154bd07a2d41
PiPi <1303798456@qq.com>
2018-12-11 00:07:36 +0800

    添加中间件解析post的res.body

--- router.js
+++ router.js
@@ -1,6 +1,9 @@
 ﻿const express = require("express");
+const multipart = require('connect-multiparty');
 const DB = require("./db");
+
 const router = express.Router();
+const multipartMiddleware = multipart();
 
 router.get("/select", (req, res) => {
   const sheet_name = req.query.sheet_name;
@@ -10,6 +13,10 @@ router.get("/select", (req, res) => {
 router.get("/selectColName", (req, res) => {
   const table_name = req.query.table_name;
   DB.col(req,res,table_name);
+});
+
+router.post('/formdata', multipartMiddleware, (req, res) => {
+  res.send(req.body);
 });
 
 module.exports = router;
